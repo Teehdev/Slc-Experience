55a33617a74d33277909a887806a234060cc8851
Your Name <atiladeoyetunji@gmail.com>
2022-04-05 18:15:58 +0100

    SlcApp

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/dashboard', ensureAuth, async (req,res)=> {
     try {
         const stories = await Story.find({ user: req.user.id}).lean()
         res.render('dashboard', {
-            name: req.user.firstName,
+            name: req.user.lastName,
             stories
         })
     } catch (err) {
